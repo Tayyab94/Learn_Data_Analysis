012d705caadcd461e08c7ce3948c5eb843209c4f
Tayyab94 <tayyab.bhatti30@gmail.com>
2025-07-13 21:09:55 +0500

    first commit

---
+++ jupyterfolder/first.py
@@ -0,0 +1,17 @@
+
+
+
+def  sum_OF_Two_Number(a: int, bn: int) -> str:
+    c = a + bn
+
+    return f"The sum of {a} and {bn} is {c}"
+
+
+result = sum_OF_Two_Number(5, 10)
+
+print(result)
+
+# if __name__ == "__main__":
+#     a = int(input("Enter first number: "))
+#     b = int(input("Enter second number: "))
+#     print(sum_OF_Two_Number(a, b))
